39cbd07a107a5150d0c0b5fcfc62f8570f04c4e5
NASH <nashon.mwenda@student.moringaschool.com>
2023-06-18 07:59:04 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
-// Code your solutions in this file
+// Function to generate thank you messages
+function writeCards(names) {
+    let thankYouMessages = [];
+    for (let i = 0; i < names.length; i++) {
+      thankYouMessages.push(`Thank you, ${names[i]}, for the wonderful surprise gift!`);
+    }
+    return thankYouMessages;
+  }
+  
+  // Function to count down from a given number
+  function countDown(number) {
+    for (let i = number; i >= 0; i--) {
+      console.log(i);
+    }
+  }
+  
+  module.exports = {
+    writeCards,
+    countDown
+  };
+  
